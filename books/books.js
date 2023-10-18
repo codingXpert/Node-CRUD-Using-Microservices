b126fb411bb71016b4df92aaa12897355ecf2d33
Vivek <vivekraj78965.vr@gmail.com>
2023-10-18 16:58:07 +0530

    database connection

--- books/books.js
+++ books/books.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+const connectDB = async () => {
+    try {
+      const db = await mongoose.connect("mongodb://localhost/bookService");
+      console.log(`connected to mongoDb ${mongoose.connection.host}`);
+    } catch (error) {
+      console.log(`MongoDB Error ${error}`);
+    }
+  };
+connectDB();
 app.get("/", (req, res) => {
     res.send("This is book service");
 })
