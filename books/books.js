1b0026c72eb6809e13dfc9010bb429bc82c74aba
Vivek <vivekraj78965.vr@gmail.com>
2023-10-18 17:17:40 +0530

    separeted the folders for book

--- books/books.js
+++ books/books.js
@@ -1,20 +1,9 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/mongoose');
+db();
 
-const connectDB = async () => {
-    try {
-      const db = await mongoose.connect("mongodb://localhost/bookService");
-      console.log(`connected to mongoDb ${mongoose.connection.host}`);
-    } catch (error) {
-      console.log(`MongoDB Error ${error}`);
-    }
-  };
-connectDB();
-app.get("/", (req, res) => {
-    res.send("This is book service");
-})
-
+app.use("/", require("./routes"));
 
 app.listen(8000, (err) => {
     if(err){
---
+++ books/controllers/bookController.js
@@ -0,0 +1,7 @@
+const getAll = (req, res) => {
+    res.send("This is book service");
+}
+
+module.exports = {
+    getAll
+}
