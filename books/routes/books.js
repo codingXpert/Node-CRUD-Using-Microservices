b94be4a50ea12543c47784bfc0f90d7212832b7d
Vivek <vivekraj78965.vr@gmail.com>
2023-10-19 15:58:04 +0530

    create customer API

--- books/routes/books.js
+++ books/routes/books.js
@@ -4,6 +4,7 @@ const bookController = require("../controllers/bookController");
 
 router.get("/getAll", bookController.getAll);
 router.get("/get/:id", bookController.getById);
+router.put("/update/:id", bookController.update);
 router.post("/create", bookController.create);
 router.delete("/delete/:id", bookController.remove);
 
