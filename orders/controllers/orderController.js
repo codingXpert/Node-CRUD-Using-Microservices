c2b270c0bda618e828760ed21e78b01a9405d35b
Vivek <vivekraj78965.vr@gmail.com>
2023-10-19 20:11:43 +0530

    get order by id with customer name and book title using axios

--- orders/controllers/orderController.js
+++ orders/controllers/orderController.js
@@ -1,4 +1,5 @@
 const Order = require('../models/orders');
+const axios = require('axios'); // user to handel http/https request for different ports
 
 const create = async (req, res) => {
     try {
@@ -26,7 +27,58 @@ const create = async (req, res) => {
     }
   };
 
+//   const getById = async (req, res) => {
+//     try {
+//       const order = await Order.findById(req.params.id);
+//       if (!order) {
+//         return res.send("No order is available");
+//       } else {
+//         const book = axios.get(`http://localhost:8000/books/get/${order.BookId}`);
+//         const customer = axios.get(`http://localhost:8001/customer/get/${order.CustomerId}`);
+//         return res.send({
+//             "_id": order.id,
+//             "CustomerId": customer.data,
+//             "BookId": book.data,
+//             "initialDate": order.initialDate,
+//             "deliveryDate": order.deliveryDate
+//         });
+//       }
+//     } catch (error) {
+//       return res.send(error);
+//     }
+//   };
+
+const getById = async (req, res) => {
+    try {
+      const order = await Order.findById(req.params.id);
+      if (!order) {
+        return res.send("No order is available");
+      } else {
+        const bookResponse = await axios.get(`http://localhost:8000/books/get/${order.BookId}`);
+        const customerResponse = await axios.get(`http://localhost:8001/customer/get/${order.CustomerId}`);
+
+        const book = bookResponse.data.author; 
+        const customer = customerResponse.data.name;
+
+        return res.send({
+            "_id": order.id,
+            "CustomerName": customer,
+            "BookTitle": book,
+            "initialDate": order.initialDate,
+            "deliveryDate": order.deliveryDate
+        });
+      }
+    } catch (error) {
+      return res.send(error);
+    }
+  };
+
+
+
+
+
   module.exports = {
     create,
-    getAll
+    getAll,
+    getById
   }
--- orders/routes/order.js
+++ orders/routes/order.js
@@ -4,5 +4,6 @@ const orderController = require("../controllers/orderController");
 
 router.post("/create", orderController.create);
 router.get("/getAll", orderController.getAll);
+router.get("/get/:id", orderController.getById);
 
 module.exports = router;
